c25ca306ca9b95fea0c02ed2d271d1f10fde1b86
Vina030 <vinasriwahyuniramadani@gmail.com>
2021-12-08 17:05:22 +0700

    Added Project

---
+++ app/src/main/java/simple/example/hewanpedia/BiodataActivity.java
@@ -0,0 +1,12 @@
+package simple.example.hewanpedia;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BiodataActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.biodata);
+    }
+}
---
+++ app/src/main/java/simple/example/hewanpedia/MainActivity.java
@@ -0,0 +1,42 @@
+package simple.example.hewanpedia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageButton btnKucing,btnAnjing;
+    public static final String JENIS_GALERI_KEY = "JENIS_GALERI";
+    private View btnSapi;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        inisialisasiView();
+
+    }
+
+    private void inisialisasiView() {
+        btnKucing = findViewById(R.id.btn_buka_ras_kucing);
+        btnAnjing = findViewById(R.id.btn_buka_ras_anjing);
+        btnSapi = findViewById(R.id.btn_buka_ras_Sapi);
+        btnKucing.setOnClickListener(view -> bukaGaleri("Kucing"));
+        btnAnjing.setOnClickListener(view -> bukaGaleri("Anjing"));
+        btnSapi.setOnClickListener(view -> bukaGaleri("Sapi"));
+
+    }
+
+    private void bukaGaleri(String jenisHewan) {
+        Log.d("MAIN","Buka activity galeri");
+        Intent intent = new Intent(this, DaftarHewanActivity.class);
+        intent.putExtra(JENIS_GALERI_KEY, jenisHewan);
+        startActivity(intent);
+    }
+
+}
